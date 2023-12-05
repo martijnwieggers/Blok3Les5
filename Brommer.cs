9627c67aad825667a3abae39db9c12131c77ab83
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2023-12-05 13:07:25 +0100

    versie voor start

---
+++ Brommer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blok3Les5
+{
+    public class Brommer
+    {
+        public string Merk { get; set; }
+
+        //Taak 1: Voeg de volgende public properties toe:
+
+        //Type (tekst)
+
+        //AantalVersnellingen (geheel getal)
+
+        //MaximumSnelheid (geheel getal)
+
+
+        //Standard Constructor, deze is er altijd
+        public Brommer() 
+        { 
+        }
+        //Constructor waar alleen merk kan worden meegegeven, de rest van de eigenschappen blijft leeg
+        public Brommer(string Merk)
+        {
+            this.Merk = Merk;
+        }
+
+        //Taak 2:
+        //Voeg een constructor toe waarbij alle waarden direct kunnen worden meegegeven
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blok3Les5;
+
+List<Brommer> Brommers = new List<Brommer>();
+
+
+//Taak 3: Maak een nieuw object aan van de Klasse brommer, geef deze de naam Brommer1
+
+
+//Taak 4: Vul de eigenschappen (Merk, Type, AantalVersnellingen en MaximumSnelheid)
+
+//Brommer1.Merk  =  
+
+//Taak 5: Voeg dit nieuw aangemaakte object toe aan de lijst Brommers
+//Brommers.Add(.....);
+
+//Taak 6: Maak nog een nieuw object van de Klasse brommer aan, geef deze de naam Brommer2
+//Gebruik bij het aanmaken van Brommer2 de constructor waarin we alle waarden direct meegeven om het object aan te maken
+
+//Taak 7: Voeg ook deze brommer toe aan de lijst met brommers, net zoals je hebt gedaan bij taak 5
+
+foreach (var brommer in Brommers)
+{
+    //Taak 8: Druk hier niet allen het merk van de brommer af, maar ook: Type, AantalVersnellingen en MaximumSnelheid
+    Console.WriteLine("Merk: " + brommer.Merk);
+}
+
+
+
+
+
+
+
+
